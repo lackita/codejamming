3af9d1c01e1222874ea03d92d4438a4e01fce24d
Colin Williams <lackita@gmail.com>
2014-04-07 08:50:42 -0400

    simplified tests and started looking for html

--- test/codejamming/test/handler.clj
+++ test/codejamming/test/handler.clj
@@ -4,27 +4,18 @@
         codejamming.handler
         crouton.html))
 
-(defn- stream [s]
-  (java.io.ByteArrayInputStream. (.getBytes s)))
+(defn status-is [r c]
+  (is (= (:status r) c)))
 
-(defn parse-string [s]
-  (parse (stream s)))
+(defn get-page [p]
+  (app (request :get p)))
 
-(defn get-user-login-page []
-  (app (request :get "/login/")))
+(deftest root-test
+  (status-is (get-page "/") 200))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
-  (testing "not-found route"
-    (let [response (app (request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+(deftest invalid-test
+  (status-is (get-page "/invalid") 404))
 
 (deftest test-user-login-page
-  ;; (pending "user logs in"
-  ;;   (get-user-login-page))
-  (testing "page contents"
-    (is (= (:status (get-user-login-page)) 200))))
+  (status-is (get-page "/login/") 200)
+  (is (re-find #"<html>.*</html>" (:body (get-page "/login/")))))
